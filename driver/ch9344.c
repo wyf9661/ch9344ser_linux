7bbc03cbd0de0969cf10c1e7c289f6b4fdb8a9f7
wangyifan <wangyifan@acoinfo.com>
2024-12-02 14:40:22 +0800

    fix build error on linux 6.12

--- driver/ch9344.c
+++ driver/ch9344.c
@@ -62,7 +62,11 @@
 #include <linux/timer.h>
 #include <linux/kfifo.h>
 #include <asm/byteorder.h>
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 12, 0))
+#include <linux/unaligned.h>
+#else
 #include <asm/unaligned.h>
+#endif
 
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 11, 0))
 #include <linux/sched/signal.h>
